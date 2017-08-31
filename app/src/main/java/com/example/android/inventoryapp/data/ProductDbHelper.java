b3f48d84feac85fa8b6f76dfcb20d37f0f9b8ab4
Vicuko <victorc.fernandez@gmail.com>
2017-08-31 12:41:03 +0200

    Create and initialize ProductDbHelper

---
+++ app/src/main/java/com/example/android/inventoryapp/data/ProductDbHelper.java
@@ -0,0 +1,48 @@
+package com.example.android.inventoryapp.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.android.inventoryapp.data.ProductContract.ProductEntry;
+
+/**
+ * Created by Vicuko on 31/8/17.
+ */
+
+public class ProductDbHelper extends SQLiteOpenHelper {
+
+    public static final String LOG_TAG = ProductDbHelper.class.getSimpleName();
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 1;
+
+    public static final String DATABASE_NAME = "inventory.db";
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + ProductEntry.TABLE_NAME + " (" +
+                    ProductEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    ProductEntry.COLUMN_PRODUCT_NAME + " TEXT NOT NULL," +
+                    ProductEntry.COLUMN_PRODUCT_DESCRIPTION + " TEXT," +
+                    ProductEntry.COLUMN_PRODUCT_PRICE + " INTEGER NOT NULL DEFAULT 0," +
+                    ProductEntry.COLUMN_PRODUCT_QUANTITY + " INTEGER NOT NULL DEFAULT 0," +
+                    ProductEntry.COLUMN_PROVIDER_NAME + " TEXT NOT NULL" +
+                    ProductEntry.COLUMN_PROVIDER_PHONE + " TEXT);";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + ProductEntry.TABLE_NAME;
+
+    public ProductDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+}
