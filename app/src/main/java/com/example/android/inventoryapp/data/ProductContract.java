aee1f4834cf36e9737f40b286066d2337507c79b
Vicuko <victorc.fernandez@gmail.com>
2017-08-31 13:07:48 +0200

    Update Contract to include MIME types

--- app/src/main/java/com/example/android/inventoryapp/data/ProductContract.java
+++ app/src/main/java/com/example/android/inventoryapp/data/ProductContract.java
@@ -1,5 +1,6 @@
 package com.example.android.inventoryapp.data;
 
+import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -23,6 +24,18 @@ public final class ProductContract {
          * The content URI to access the product data in the provider
          */
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PRODUCTS);
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of products.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single products.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
 
         public static final String _ID = BaseColumns._ID;
         public static final String COLUMN_PRODUCT_NAME = "name";
