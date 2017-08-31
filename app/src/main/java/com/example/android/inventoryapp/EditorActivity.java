702861b7e1dc3b056630b4fa6efc868056105962
Vicuko <victorc.fernandez@gmail.com>
2017-08-31 14:34:42 +0200

    Initialize variables in Editor Activity

--- app/src/main/java/com/example/android/inventoryapp/EditorActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/EditorActivity.java
@@ -1,9 +1,22 @@
 package com.example.android.inventoryapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class EditorActivity extends AppCompatActivity {
+
+    private static final int PET_LOADER = 0;
+    private EditText mNameEditText;
+    private EditText mDescriptionEditText;
+    private EditText mPriceEditText;
+    private EditText mQuantityEditText;
+    private EditText mProviderNameEditText;
+    private EditText mProviderPhoneEditText;
+
+    private Uri mCurrentPetUri;
+    private boolean mProductHasChanged = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
