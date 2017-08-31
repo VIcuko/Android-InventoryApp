e21cfc943d0126b36e3f6559b4df08754d0c4f0a
Vicuko <victorc.fernandez@gmail.com>
2017-08-31 14:14:11 +0200

    Override onCreateOptionsMenu and onOptionsItemSelected to handle menu in Inventory Activity class

--- app/src/main/java/com/example/android/inventoryapp/InventoryActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/InventoryActivity.java
@@ -9,6 +9,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -56,7 +58,7 @@ public class InventoryActivity extends AppCompatActivity implements LoaderManage
             }
         });
 
-
+        getLoaderManager().initLoader(PRODUCT_LOADER, null, this);
     }
 
     @Override
@@ -73,4 +75,21 @@ public class InventoryActivity extends AppCompatActivity implements LoaderManage
     public void onLoaderReset(Loader<Cursor> loader) {
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_inventory, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_delete_all_entries:
+//                showDeleteConfirmationDialog();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
