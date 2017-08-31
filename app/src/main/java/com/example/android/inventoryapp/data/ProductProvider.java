a66e21c85e678b631cf41575772a6555dfc8240e
Vicuko <victorc.fernandez@gmail.com>
2017-08-31 13:37:57 +0200

    Define all methods for ProductProvider class

--- app/src/main/java/com/example/android/inventoryapp/data/ProductProvider.java
+++ app/src/main/java/com/example/android/inventoryapp/data/ProductProvider.java
@@ -1,47 +1,227 @@
 package com.example.android.inventoryapp.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.example.android.inventoryapp.data.ProductContract.ProductEntry;
 
 /**
  * Created by Vicuko on 31/8/17.
  */
 
 public class ProductProvider extends ContentProvider {
+
+    /**
+     * URI matcher code for the content URI for the products table
+     **/
+    private static final int PRODUCTS = 100;
+
+    /**
+     * URI matcher code for the content URI for a single PRODUCT in the products table
+     **/
+    private static final int PRODUCT_ID = 101;
+
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        //These are all the possible paths we are going to deal when receiving a call to the database.
+        sUriMatcher.addURI(ProductContract.CONTENT_AUTHORITY, ProductContract.PATH_PRODUCTS, PRODUCTS);
+        sUriMatcher.addURI(ProductContract.CONTENT_AUTHORITY, ProductContract.PATH_PRODUCTS + "/#", PRODUCT_ID);
+    }
+
+    private ProductDbHelper mDbHelper;
+
+    public static final String LOG_TAG = ProductProvider.class.getSimpleName();
+
     @Override
     public boolean onCreate() {
-        return false;
+        mDbHelper = new ProductDbHelper(getContext());
+        return true;
     }
 
     @Nullable
     @Override
-    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
-        return null;
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection,
+                        @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        Cursor cursor;
+
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PRODUCTS:
+                cursor = database.query(ProductEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            case PRODUCT_ID:
+                selection = ProductContract.ProductEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                cursor = database.query(ProductEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        return cursor;
+
     }
 
     @Nullable
     @Override
     public String getType(@NonNull Uri uri) {
-        return null;
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PRODUCTS:
+                return ProductEntry.CONTENT_LIST_TYPE;
+            case PRODUCT_ID:
+                return ProductEntry.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalStateException("Unknown URI " + uri + " with match " + match);
+        }
     }
 
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
-        return null;
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PRODUCTS:
+                return insertProduct(uri, contentValues);
+            default:
+                throw new IllegalArgumentException("Insertion is not supported for " + uri);
+        }
+    }
+
+    private Uri insertProduct(Uri uri, ContentValues values) {
+
+        String name = values.getAsString(ProductEntry.COLUMN_PRODUCT_NAME);
+        int price = values.getAsInteger(ProductEntry.COLUMN_PRODUCT_PRICE);
+        int quantity = values.getAsInteger(ProductEntry.COLUMN_PRODUCT_QUANTITY);
+        String providerName = values.getAsString(ProductEntry.COLUMN_PROVIDER_NAME);
+
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Product requires a name");
+        }
+
+        if (price < 0) {
+            throw new IllegalArgumentException("Price can\'t be negative");
+        }
+
+        if (quantity < 0) {
+            throw new IllegalArgumentException("Quantity can\'t be negative");
+        }
+
+        if (providerName == null || providerName.isEmpty()) {
+            throw new IllegalArgumentException("Provider requires a name");
+        }
+
+        // Get readable database
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        long newRowId = database.insert(ProductEntry.TABLE_NAME, null, values);
+
+        if (newRowId == -1) {
+            Log.e(LOG_TAG, "Failed to insert row for " + uri);
+            return null;
+        }
+
+        // Notify all listeners that the data has changed for the product content URI
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        // Once we know the ID of the new row in the table,
+        // return the new URI with the ID appended to the end of it
+        return ContentUris.withAppendedId(uri, newRowId);
     }
 
     @Override
-    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
-        return 0;
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        int rowsDeleted;
+
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+
+            case PRODUCTS:
+                // Save number of rows deleted that match the selection and selection args after deleting them
+                rowsDeleted = database.delete(ProductEntry.TABLE_NAME, selection, selectionArgs);
+                if (rowsDeleted != 0) {
+                    getContext().getContentResolver().notifyChange(uri, null);
+                }
+                return rowsDeleted;
+
+            case PRODUCT_ID:
+                // Delete a single row given by the ID in the URI
+                selection = ProductEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                rowsDeleted = database.delete(ProductEntry.TABLE_NAME, selection, selectionArgs);
+                if (rowsDeleted != 0) {
+                    getContext().getContentResolver().notifyChange(uri, null);
+                }
+                return rowsDeleted;
+            default:
+                throw new IllegalArgumentException("Deletion is not supported for " + uri);
+        }
     }
 
     @Override
-    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
-        return 0;
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        if (values.size() == 0) {
+            return 0;
+        }
+
+        String name = values.getAsString(ProductEntry.COLUMN_PRODUCT_NAME);
+        int price = values.getAsInteger(ProductEntry.COLUMN_PRODUCT_PRICE);
+        int quantity = values.getAsInteger(ProductEntry.COLUMN_PRODUCT_QUANTITY);
+        String providerName = values.getAsString(ProductEntry.COLUMN_PROVIDER_NAME);
+
+        if (values.containsKey(ProductEntry.COLUMN_PROVIDER_NAME)) {
+            if (name == null || name.isEmpty()) {
+                throw new IllegalArgumentException("Product requires a name");
+            }
+        }
+
+        if (values.containsKey(ProductEntry.COLUMN_PRODUCT_PRICE)) {
+            if (price < 0) {
+                throw new IllegalArgumentException("Price can\'t be negative");
+            }
+        }
+
+        if (values.containsKey(ProductEntry.COLUMN_PRODUCT_QUANTITY)) {
+            if (quantity < 0) {
+                throw new IllegalArgumentException("Quantity can\'t be negative");
+            }
+        }
+
+        if (values.containsKey(ProductEntry.COLUMN_PROVIDER_NAME)) {
+            if (providerName == null || providerName.isEmpty()) {
+                throw new IllegalArgumentException("Provider requires a name");
+            }
+        }
+
+        // Get readable database
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        int rowsUpdated = database.update(ProductEntry.TABLE_NAME, values, selection, selectionArgs);
+
+        // If 1 or more rows were updated, then notify all listeners that the data at the
+        // given URI has changed
+        if (rowsUpdated != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return rowsUpdated;
     }
 }
