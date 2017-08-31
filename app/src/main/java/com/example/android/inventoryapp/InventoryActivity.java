9894afd566796dfd3c72d3eebe878c70f616dd77
Vicuko <victorc.fernandez@gmail.com>
2017-08-31 14:07:40 +0200

    Initialize static fina int variable for loader id and fab to carry out intent to editor activity + associate adapter to listview and make each item clickable to create intent to EditorActivity

--- app/src/main/java/com/example/android/inventoryapp/InventoryActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/InventoryActivity.java
@@ -1,13 +1,76 @@
 package com.example.android.inventoryapp;
 
+import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class InventoryActivity extends AppCompatActivity {
+import com.example.android.inventoryapp.data.ProductContract.ProductEntry;
+
+public class InventoryActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final int PRODUCT_LOADER = 0;
+
+    ProductCursorAdapter mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(InventoryActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ListView inventoryListView = (ListView) findViewById(R.id.inventory_list_view);
+        View emptyView = findViewById(R.id.empty_view);
+        inventoryListView.setEmptyView(emptyView);
+
+        mCursorAdapter = new ProductCursorAdapter(this, null);
+        inventoryListView.setAdapter(mCursorAdapter);
+
+        inventoryListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Intent intent = new Intent(InventoryActivity.this, EditorActivity.class);
+
+                Uri currentUri = ContentUris.withAppendedId(ProductEntry.CONTENT_URI, id);
+
+                intent.setData(currentUri);
+
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
