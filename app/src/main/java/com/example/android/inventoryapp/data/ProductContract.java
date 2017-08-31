7c18ea727e52061f3632024fe52d92c261fa1df6
Vicuko <victorc.fernandez@gmail.com>
2017-08-31 12:21:58 +0200

    Delet mime objects from ProductContract class

--- app/src/main/java/com/example/android/inventoryapp/data/ProductContract.java
+++ app/src/main/java/com/example/android/inventoryapp/data/ProductContract.java
@@ -1,6 +1,5 @@
 package com.example.android.inventoryapp.data;
 
-import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -24,19 +23,6 @@ public final class ProductContract {
          * The content URI to access the product data in the provider
          */
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PRODUCTS);
-
-        /**
-         * The MIME type of the {@link #CONTENT_URI} for a list of products.
-         */
-        public static final String CONTENT_LIST_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
-
-        /**
-         * The MIME type of the {@link #CONTENT_URI} for a single product.
-         */
-        public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
-
 
         public static final String _ID = BaseColumns._ID;
         public static final String COLUMN_PRODUCT_NAME = "name";
