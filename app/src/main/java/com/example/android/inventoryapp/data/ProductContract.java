ad48de2f57948966c1d7ab89715eea2a9d6b7f89
Vicuko <victorc.fernandez@gmail.com>
2017-08-31 12:20:10 +0200

    Create and define ProductContract class

---
+++ app/src/main/java/com/example/android/inventoryapp/data/ProductContract.java
@@ -0,0 +1,69 @@
+package com.example.android.inventoryapp.data;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by Vicuko on 31/8/17.
+ */
+
+public final class ProductContract {
+
+    public static final String CONTENT_AUTHORITY = "com.example.android.inventoryapp";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_PRODUCTS = "products";
+
+    public static abstract class ProductEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "products";
+
+        /**
+         * The content URI to access the product data in the provider
+         */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PRODUCTS);
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of products.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single product.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
+
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PRODUCT_NAME = "name";
+        public static final String COLUMN_PRODUCT_DESCRIPTION = "description";
+        public static final String COLUMN_PRODUCT_PRICE = "price";
+        public static final String COLUMN_PRODUCT_QUANTITY = "quantity";
+        public static final String COLUMN_PROVIDER_NAME = "provider_name";
+        public static final String COLUMN_PROVIDER_PHONE = "provider_phone";
+
+        /*
+        Initial price to be shown for new products
+         */
+        public static final int PRICE_INITIAL = 0;
+
+        public static boolean isValidPrice(int price) {
+            if (price >= 0) {
+                return true;
+            }
+            return false;
+        }
+
+        public static boolean isValidQuantity(int quantity) {
+            if (quantity >= 0) {
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
